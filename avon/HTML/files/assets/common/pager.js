3bc6f692c2eea62e682696ba0a94af413ab247c3
Fred Guth <fredguth@fredguth.com>
2017-05-19 21:57:04 -0300

    initial commit

---
+++ avon/HTML/files/assets/common/pager.js
@@ -0,0 +1,85 @@
+{
+    "bookSize": {
+        "width": "790", "height": "1200"
+    }
+    ,
+    "links": {
+        "color": "#3FC7FF", "target": "_blank", "highlightOnFlip": true, "highlightOnHover": true
+    }
+    ,
+    "search": {
+        "color": "#FFE921"
+    }
+    ,
+    "hardcover":false,
+    "hardcoverColor":"#000000",
+    "flipCorner":false,
+    "rightToLeft":false,
+    "numerationOffset":0,
+    "pageFlippingDuration":0.8,
+    "pages": {
+        "defaults": {
+            "backgroundImageWidth": 0, "backgroundImageHeight": 0, "contentWidth": 0, "width": 1580, "height": 1200, "thFormat": "jpg", "textLayer": true, "substrateFormat": "jpg", "substrateZoomFormat": "jpg", "stub": false, "contentScale": 0.9875, "slideDelay": 2, "backgroundColor": "#FFFFFF", "wide": false, "pageResize": "FIT"
+        }
+        ,
+        "1": {
+            "thFormat": "png", "width": 790
+        }
+        ,
+        "2": {
+            "wide": true, "thFormat": "png"
+        }
+        ,
+        "3": {
+            "wide": true, "thFormat": "png"
+        }
+        ,
+        "4": {
+            "wide": true, "thFormat": "png"
+        }
+        ,
+        "5": {
+            "wide": true, "thFormat": "png"
+        }
+        ,
+        "6": {
+            "wide": true, "thFormat": "png"
+        }
+        ,
+        "7": {
+            "wide": true, "thFormat": "png"
+        }
+        ,
+        "8": {
+            "wide": true, "thFormat": "png"
+        }
+        ,
+        "9": {
+            "wide": true, "thFormat": "png"
+        }
+        ,
+        "10": {
+            "wide": true, "thFormat": "png"
+        }
+        ,
+        "structure":["1",
+        "2",
+        null,
+        "3",
+        null,
+        "4",
+        null,
+        "5",
+        null,
+        "6",
+        null,
+        "7",
+        null,
+        "8",
+        null,
+        "9",
+        null,
+        "10",
+        null]
+    }
+}
